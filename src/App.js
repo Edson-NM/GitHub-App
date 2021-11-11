97d32af3945416b38a4034c6c3fc096f251200e8
Edson-NM <edson_nogueda@outlook.com>
2021-11-11 00:26:15 -0600

    Adicion de React Router y Tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+
+//COMPONENTS
+import Header from './Components/Custom/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Header/>
+            {/* <Home/> */}
+          </Route>
+          <Route path="/" exact>
+
+          </Route>
+          <Route path="*">
+            <h2>404 Not Found</h2>
+          </Route>
+
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/Components/Custom/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="h-24 bg-yellow-400">
+            
+        </div>
+    );
+};
+
+export default Header;
